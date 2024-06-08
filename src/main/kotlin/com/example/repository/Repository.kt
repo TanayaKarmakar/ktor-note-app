7e1f6753af68ce97311af8f42428259a00ad1e2e
tanayakarmakar <karmakar.tanaya@gmail.com>
2024-06-08 10:55:42 +0530

    crud note

--- src/main/kotlin/com/example/repository/Repository.kt
+++ src/main/kotlin/com/example/repository/Repository.kt
@@ -1,11 +1,11 @@
 package com.example.repository
 
+import com.example.models.entity.Note
 import com.example.models.entity.User
+import com.example.models.tables.NoteTable
 import com.example.models.tables.UserTable
 import com.example.repository.DatabaseFactory.dbQuery
-import org.jetbrains.exposed.sql.ResultRow
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.*
 
 class Repository {
     suspend fun addUser(user: User) =
@@ -32,6 +32,56 @@ class Repository {
                 email = row[UserTable.email],
                 name = row[UserTable.name],
                 hashPassword = row[UserTable.hashPassword]
+        )
+    }
+
+
+    suspend fun addNote(note: Note, email: String) {
+        dbQuery {
+            NoteTable.insert { nt ->
+                nt[NoteTable.id] = note.id
+                nt[NoteTable.userEmail] = email
+                nt[NoteTable.noteTitle] = note.noteTitle
+                nt[NoteTable.description] = note.description
+                nt[NoteTable.date] = note.date
+            }
+        }
+    }
+
+    suspend fun getAllNotes(email: String): List<Note> = dbQuery {
+        NoteTable.select{
+            NoteTable.userEmail.eq(email)
+        }.mapNotNull { rowToNote(it) }
+    }
+
+    suspend fun updateNote(note: Note, email: String) {
+        dbQuery {
+            NoteTable.update(
+                    where = {
+                        NoteTable.userEmail.eq(email) and NoteTable.id.eq(note.id)
+                    }
+            ) { nt->
+                nt[NoteTable.noteTitle] = note.noteTitle
+                nt[NoteTable.description] = note.description
+                nt[NoteTable.date] = note.date
+            }
+        }
+    }
+
+    suspend fun deleteNote(id: String) {
+        dbQuery {
+            NoteTable.deleteWhere { NoteTable.id.eq(id) }
+        }
+    }
+
+    private fun rowToNote(row: ResultRow?): Note? {
+        if(row == null)
+            return null
+        return Note(
+                id = row[NoteTable.id],
+                noteTitle = row[NoteTable.noteTitle],
+                description = row[NoteTable.description],
+                date = row[NoteTable.date]
         )
     }
 }
